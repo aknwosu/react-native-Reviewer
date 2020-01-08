f455ca852c493b683d027395a7caa20d2430a94d
Rebecca Nwosu <akunnarebecca@gmail.com>
2020-01-08 14:54:39 +0100

    header images

--- screens/home.js
+++ screens/home.js
@@ -6,8 +6,8 @@ import Card from '../shared/card'
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
     { title: 'Zelda, breath of fresh air', rating: '5', body: 'lorem ipsum', key: '1' },
-    { title: 'Gotta Catch them all', rating: '5', body: 'lorem ipsum', key: '2' },
-    { title: 'Not so final fantasy', rating: '5', body: 'lorem ipsum', key: '3'}
+    { title: 'Gotta Catch them all', rating: '3', body: 'lorem ipsum', key: '2' },
+    { title: 'Not so final fantasy', rating: '1', body: 'lorem ipsum', key: '3'}
   ])
   return (
     <View style={globalStyles.container}>
--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Header({navigation, title }) {
@@ -8,12 +8,13 @@ export default function Header({navigation, title }) {
   }
 
   return(
-    <View style={styles.header}>
+    <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
       <MaterialIcons name='menu' size={28} style={styles.icon} onPress={openMenu} />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image style={styles.headerImage} source={require('../assets/heart_logo.png')} />
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </View>
+    </ImageBackground>
   )
 }
 const styles = StyleSheet.create({
@@ -32,6 +33,14 @@ const styles = StyleSheet.create({
   },
   icon: {
     position: 'absolute',
-    left: 16
+    left: 16,
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10
+  },
+  headerTitle: {
+    flexDirection: 'row'
   }
 })
